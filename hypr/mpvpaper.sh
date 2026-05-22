a8e359565fb673adb6d9828b73e671f3e65be245
Matéo LEROY <mateoleroy084@gmail.com>
2026-05-22 11:09:46 +0200

    binding

--- hypr/mpvpaper.sh
+++ hypr/mpvpaper.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+# Tuer toute instance existante pour repartir sur une surface propre
+pkill -x mpvpaper
 # Attendre que hyprland réponde
 while ! hyprctl monitors &>/dev/null; do
-    sleep 0.5
+    sleep 0.1
 done
 # Attendre un monitor spécifique
-sleep 1
-mpvpaper -o "no-audio --loop --video-unscaled=no --panscan=1.0" '*' "~/Images/wallpapers/frog2.mp4"
+sleep 0.2
+mpvpaper -o "no-audio --loop --video-unscaled=no --panscan=1.0" '*' "$HOME/Images/wallpapers/frog2.mp4"
